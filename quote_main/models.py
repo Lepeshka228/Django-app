312ffe9c8902bcf7043a85390bd936c76f01af83
Alexandr <romanovsasa26@gmail.com>
2025-08-25 19:29:36 +0300

    наследие шаблонов

--- quote_main/models.py
+++ quote_main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# class QuoteSource(models.Model):
+#     type = models.CharField(max_length=20)
+#     name = models.CharField(max_length=100)
+#     count_quotes = models.IntegerField()
+#     def __str__(self):
+#         return self.name
+
+
--- quote_main/views.py
+++ quote_main/views.py
@@ -1,17 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
 # переменная request хранит в себе все данные запроса 
 # httpresponse возвращает все данные ответа 
 def index(request):
 
     context: dict[str, str] = {
         'title': 'Цитатник',
-        'content': 'Здесь будет показываться цитата'
+        'name_of_quote': 'Это великая цитата великого философа, которая навсегда останется в наших умах',
+        'type_of_source': 'Книга',
+        'name_of_source': 'Государство',
+        'author_of_source': 'Платон'
     }
 
-    return render(request, 'main/base.html', context)
+    return render(request, 'main/index.html', context)
 
 def top_ten(request):
     return HttpResponse('top_ten')
