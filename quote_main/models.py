39402f97b7b68c66bfad860bf9bef73198e99941
Alexandr <romanovsasa26@gmail.com>
2025-08-26 12:14:13 +0300

    2 модельки

--- quote_main/models.py
+++ quote_main/models.py
@@ -1,10 +1,43 @@
 from django.db import models
 
-# class QuoteSource(models.Model):
-#     type = models.CharField(max_length=20)
-#     name = models.CharField(max_length=100)
-#     count_quotes = models.IntegerField()
-#     def __str__(self):
-#         return self.name
+class QuoteSources(models.Model):
+    """ Модель источника цитаты - название, тип, автор """
+
+    # типы источников
+    TYPE_CHOICE = (
+        ('film', 'Фильм'),
+        ('book', 'Книга'),
+        ('song', 'Песня')
+    )
+    # название источника
+    name = models.CharField(max_length=100)
+    # тип источника
+    type = models.CharField(max_length=20, 
+                            choices=TYPE_CHOICE)
+    # автор источника
+    author = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'sources'
+
+    def __str__(self):
+        return self.name
+
+
+class Quotes(models.Model):
+    """ Модель цитаты - цитата, вес, лайки, дизлайки, просмотры, источник(внеш.кл.)"""
+
+    WEIGHT_CHOICE = (1, 2, 3, 4, 5)
+
+    quote = models.TextField(unique=True)
+    weight = models.IntegerField(choices=WEIGHT_CHOICE)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
+    views = models.IntegerField()
+    source = models.ForeignKey(to='QuoteSources', verbose_name='Источник')
+
+    class Meta:
+        db_table = 'quotes'
+
 
 
