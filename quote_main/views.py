cf6a5961a0565218cef4c00e3745a9642a14a769
Alexandr <romanovsasa26@gmail.com>
2025-08-26 16:43:51 +0300

    подправлена view top_10 и шаблон

--- quote_main/views.py
+++ quote_main/views.py
@@ -8,6 +8,7 @@ def index(request):
     context: dict[str, str] = {
         'title': 'Цитатник',
         'name_of_quote': 'Это великая цитата великого философа, которая навсегда останется в наших умах',
+        'count_of_views': 2,
         'type_of_source': 'Книга',
         'name_of_source': 'Государство',
         'author_of_source': 'Платон'
@@ -16,4 +17,9 @@ def index(request):
     return render(request, 'main/index.html', context)
 
 def top_ten(request):
-    return render(request, 'main/top_ten.html')
+
+    context: dict[str, str] = {
+        'title': 'Топ 10 цитат',
+    }
+
+    return render(request, 'main/top_ten.html', context)
